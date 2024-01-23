38839c74cba260863e91563f6e0b7bca94822787
gabrielppd77 <gabrielppd77@outlook.com>
2024-01-23 20:53:03 -0300

    add alias path configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Sidebar } from "./components/Sidebar";
-import { Home } from "./pages/Home";
+import { Sidebar } from "@components/Sidebar";
+import { Home } from "@pages/Home";
 
 export default function App() {
   return (
-    <div className="grid min-h-screen grid-cols-app">
+    <div className="grid-cols-app grid min-h-screen">
       <Sidebar />
       <main className="px-8 pb-12 pt-8">
         <Home />
