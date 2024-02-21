82dce23c707cd6ab8380112fcb22e22031ee2656
gabrielppd77 <gabrielppd77@outlook.com>
2024-02-20 21:57:12 -0300

    finish actions table, edit an delet

--- src/entities/supplier/requests/update.ts
+++ src/entities/supplier/requests/update.ts
@@ -1,5 +1,6 @@
 import { api } from "@lib/api";
 
 export async function update(data: { id: string; name: string }) {
-  await api.put("/suppliers/" + data.id, data);
+  const { id, ...rest } = data;
+  await api.put("/suppliers/" + id, rest);
 }
--- src/pages/Supplier/Table/index.tsx
+++ src/pages/Supplier/Table/index.tsx
@@ -1,11 +1,22 @@
 import { DataTable } from "@components/DataTable";
 
-import { useSupplierQuery } from "@entities/supplier/useSupplier";
+import {
+  useSupplierMutate,
+  useSupplierQuery,
+} from "@entities/supplier/useSupplier";
 import { useTableSearchParams } from "@hooks/useTableSearchParams";
+import { Pencil, Trash2 } from "lucide-react";
+
+import { confirmDelete } from "@lib/alert";
+import { TriggerDialog } from "@components/TriggerDialog";
+import { Form } from "../Form";
+import { IconButton } from "@components/IconButton";
 
 export function Table() {
   const { pagination } = useTableSearchParams();
   const { data, isLoading, isFetching } = useSupplierQuery(pagination);
+
+  const { mutateAsyncDelete } = useSupplierMutate();
 
   return (
     <DataTable
@@ -17,6 +28,36 @@ export function Table() {
         {
           header: "Nome",
           accessorKey: "name",
+        },
+        {
+          header: "Ações",
+          enableSorting: false,
+          size: 120,
+          cell: ({ row }) => {
+            const data = row.original;
+            return (
+              <div className="flex items-center justify-center gap-4">
+                <TriggerDialog
+                  title="Atualizar Fornecedor"
+                  trigger={
+                    <IconButton>
+                      <Pencil />
+                    </IconButton>
+                  }
+                >
+                  {(close) => <Form close={close} data={data} />}
+                </TriggerDialog>
+
+                <IconButton
+                  onClick={() =>
+                    confirmDelete(() => mutateAsyncDelete(data.id))
+                  }
+                >
+                  <Trash2 />
+                </IconButton>
+              </div>
+            );
+          },
         },
       ]}
       searchOptions={[
